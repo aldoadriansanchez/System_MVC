1bfa2562583ece367329f6f1a2808f31d73984cb
Ing. Aldo A. Sanchez G <aldoasanchezg@gmail.com>
2021-10-07 14:35:18 -0500

    Create App.php
    Configuración que tendrá la aplicación en los navegadores y el servidor local

---
+++ Config/App.php
@@ -0,0 +1,34 @@
+<?php
+
+   const SERVERURL="http://localhost/System_MVC/";
+   const COMPANY="SISTEMAS PRESTAMO";
+   CONST MONEDA="$";
+   date_default_timezone_set("America/Mexico_City");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

28edec764b83e2e2f827bc5c6c404fc8365e9a11
Ing. Aldo A. Sanchez G <aldoasanchezg@gmail.com>
2021-10-07 14:33:02 -0500

    Create Server.php
    Se hace la configuración de los parámetros  y métodos de seguridad del usuario con su interacción a la base de datos

---
+++ Config/Server.php
@@ -0,0 +1,12 @@
+<?php
+const Server="localhost";
+const DB="prestamos";
+const USER="root";
+const Pass="";
+const sgbd="mysql:host=".Server.";dbname=".DB;
+const Method ="AES-256-CBC";
+const Secret_key='$aldo@1197';
+const secret_iv='037970';
+
+
+?>
